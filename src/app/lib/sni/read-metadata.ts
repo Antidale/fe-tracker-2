89a86a08b2b0c351e3f2ce633094445ee76e003d
Antidale <antidale@tellah.life>
2025-06-01 22:26:17 -0700

    refactor and code-cleanup a bit

--- src/app/lib/sni/read-metadata.ts
+++ src/app/lib/sni/read-metadata.ts
@@ -3,6 +3,7 @@ import { DeviceMemoryClient } from "@/app/sni/sni.client";
 import { GrpcWebFetchTransport } from "@protobuf-ts/grpcweb-transport";
 
 import { memoryAddresses, MemoryAddressName } from "../default-data";
+import { MemoryAddress } from "../interfaces";
 
 const getTransport = (port: string, host: string) => {
     return new GrpcWebFetchTransport({
@@ -10,32 +11,18 @@ const getTransport = (port: string, host: string) => {
     })
 }
 
-async function readMetadataDocLength(client: DeviceMemoryClient, uri: string, addressSpace: AddressSpace) {
-    const stuff = await client.singleRead({
+async function performSingleRead(client: DeviceMemoryClient, uri: string, addressSpace: AddressSpace, memoryAddress: MemoryAddress, overrideLength?: number) {
+    const readResult = await client.singleRead({
         uri: uri,
         request: {
             requestMemoryMapping: MemoryMapping.Unknown,
-            size: 4,
+            size: overrideLength ?? memoryAddress.size,
             requestAddressSpace: addressSpace,
-            requestAddress: memoryAddresses[MemoryAddressName.MetadataLength].address
+            requestAddress: memoryAddress.address
         }
     })
 
-    return stuff.response;
-}
-
-async function readMetadataDoc(client: DeviceMemoryClient, uri: string, addressSpace: AddressSpace, docLength: number) {
-    const stuff = await client.singleRead({
-        uri: uri,
-        request: {
-            requestMemoryMapping: MemoryMapping.Unknown,
-            size: docLength,
-            requestAddressSpace: addressSpace,
-            requestAddress: memoryAddresses[MemoryAddressName.MetadataDocument].address
-        }
-    })
-
-    return stuff.response;
+    return readResult
 }
 
 function convert(input: Uint8Array<ArrayBufferLike>) {
@@ -46,24 +33,30 @@ function convert(input: Uint8Array<ArrayBufferLike>) {
 }
 
 export async function readMetadata(uri: string | undefined, addressSpace: AddressSpace | undefined, port: string, host: string) {
-    if (!uri) { return "" }
+    if (!uri) { return {} }
 
     if (addressSpace === undefined) {
         addressSpace = AddressSpace.FxPakPro
     }
 
     const transport = getTransport(port, host);
-    const thingy = new DeviceMemoryClient(transport);
-    const stuff = await readMetadataDocLength(thingy, uri, addressSpace);
-    if (!stuff.response) {
-        return ""
+    const memoryClient = new DeviceMemoryClient(transport);
+    const docLengthResponse = await performSingleRead(
+        memoryClient,
+        uri,
+        addressSpace,
+        memoryAddresses[MemoryAddressName.MetadataLength]
+    );
+
+    if (!docLengthResponse.response.response) {
+        return {}
     }
 
-    const junk = convert(stuff.response.data);
+    const docLength = convert(docLengthResponse.response.response.data);
 
-    const documentResponse = await readMetadataDoc(thingy, uri, addressSpace, junk);
+    const documentResponse = await performSingleRead(memoryClient, uri, addressSpace, memoryAddresses[MemoryAddressName.MetadataDocument], docLength);
 
-    const blargh = new TextDecoder("utf-8").decode(documentResponse.response?.data)
+    const metadataDocumentString = new TextDecoder("utf-8").decode(documentResponse.response.response?.data)
 
-    return JSON.parse(blargh)
+    return JSON.parse(metadataDocumentString)
 }
