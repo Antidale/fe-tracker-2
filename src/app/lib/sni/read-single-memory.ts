1ba1f037c5c0a9a113dc244516b3192294148fa9
Antidale <antidale@tellah.life>
2025-06-01 22:31:49 -0700

    more refactoring

---
+++ src/app/lib/sni/read-single-memory.ts
@@ -0,0 +1,18 @@
+import { AddressSpace, MemoryMapping } from "@/app/sni/sni"
+import { DeviceMemoryClient } from "@/app/sni/sni.client"
+import { MemoryAddress } from "../interfaces"
+
+
+export async function performSingleRead(client: DeviceMemoryClient, uri: string, addressSpace: AddressSpace, memoryAddress: MemoryAddress, overrideLength?: number) {
+    const readResult = await client.singleRead({
+        uri: uri,
+        request: {
+            requestMemoryMapping: MemoryMapping.Unknown,
+            size: overrideLength ?? memoryAddress.size,
+            requestAddressSpace: addressSpace,
+            requestAddress: memoryAddress.address
+        }
+    })
+
+    return readResult
+}
