c0fc061d7d9f922af087c97baf28f2ac6e1a002c
Antidale <antidale@tellah.life>
2025-06-01 21:55:10 -0700

    initial reading of metadata

--- src/app/lib/sni/read-metadata.ts
+++ src/app/lib/sni/read-metadata.ts
@@ -24,6 +24,27 @@ async function readMetadataDocLength(client: DeviceMemoryClient, uri: string, ad
     return stuff.response;
 }
 
+async function readMetadataDoc(client: DeviceMemoryClient, uri: string, addressSpace: AddressSpace, docLength: number) {
+    const stuff = await client.singleRead({
+        uri: uri,
+        request: {
+            requestMemoryMapping: MemoryMapping.Unknown,
+            size: docLength,
+            requestAddressSpace: addressSpace,
+            requestAddress: memoryAddresses[MemoryAddressName.MetadataDocument].address
+        }
+    })
+
+    return stuff.response;
+}
+
+function convert(input: Uint8Array<ArrayBufferLike>) {
+    const length = input.length;
+    const buff = Buffer.from(input);
+    const result = buff.readUIntLE(0, length)
+    return result;
+}
+
 export async function readMetadata(uri: string | undefined, addressSpace: AddressSpace | undefined, port: string, host: string) {
     if (!uri) { return "" }
 
@@ -34,7 +55,15 @@ export async function readMetadata(uri: string | undefined, addressSpace: Addres
     const transport = getTransport(port, host);
     const thingy = new DeviceMemoryClient(transport);
     const stuff = await readMetadataDocLength(thingy, uri, addressSpace);
+    if (!stuff.response) {
+        return ""
+    }
 
+    const junk = convert(stuff.response.data);
 
-    return stuff.response?.data.toString()
+    const documentResponse = await readMetadataDoc(thingy, uri, addressSpace, junk);
+
+    const blargh = new TextDecoder("utf-8").decode(documentResponse.response?.data)
+
+    return JSON.parse(blargh)
 }
