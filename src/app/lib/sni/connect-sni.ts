54d8bfb30280c7ac927f9a42db20e5a0e1bb129f
Antidale <antidale@tellah.life>
2025-06-01 22:44:35 -0700

    inconsequential cleanup (spacing, semicolons)

--- src/app/lib/sni/connect-sni.ts
+++ src/app/lib/sni/connect-sni.ts
@@ -1,11 +1,10 @@
 import { GrpcWebFetchTransport } from "@protobuf-ts/grpcweb-transport";
 import { DevicesClient } from "../../sni/sni.client";
 
-
 export async function connectSni(portStr: string, host = "localhost", attempts = 0) {
     const portInt = parseInt(portStr);
     if (isNaN(portInt)) {
-        console.error()
+        console.error(`invalid port given: ${portStr}`)
         return Promise.reject(new Error(`sni port ${portStr} turned to NaN`));
     }
 
@@ -14,19 +13,22 @@ export async function connectSni(portStr: string, host = "localhost", attempts =
 
     attempts += 1;
 
+    //TODO: figure out errors on very first connection to supernt (and maybe emulators, haven't tried them yet).
     try {
         const channel = new GrpcWebFetchTransport(({ baseUrl: `http://${host}:${portInt}` }));
         const devicesClient = new DevicesClient(channel);
         const listedDevices = await devicesClient.listDevices({ kinds: [] });
         switch (listedDevices.response.devices.length) {
-            //it's possible that something just isn't running quite yet. This, and the catch block, are some shots in the dark. Still probably need/want a reconnect button if things ever get disconnected.
-            case 0: return await connectSni(portStr, host, attempts)
+            case 0: {
+                return await connectSni(portStr, host, attempts)
+            }
 
             //Just give them the first one back, if need be later on we can add something to let them select a device
             default: return Promise.resolve(listedDevices.response.devices[0]);
         }
 
     } catch {
+
         return await connectSni(portStr, host, attempts)
     }
 }
